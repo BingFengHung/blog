a202f0cc968bc3e6eadfc63e8dc484b6e0651f1a
BingFengHung <joe94008@gmail.com>
2024-05-28 05:27:05 +0800

    <feat>: articles 加入 handleArticleData 方法，用來解決 url 路徑解析問題

--- store/articles.js
+++ store/articles.js
@@ -3,9 +3,43 @@ import { defineStore } from 'pinia'
 function getArticleCount() {
   const data = this.articleData
   this.articleCount = Object.keys(data).reduce((pre, cur) => {
-    pre + data[cur].length
+    pre += data[cur].length
     return pre
   }, 0)
+}
+
+function convertLinkURL(url) {
+    const parseUrl = new URL(url)
+    const baseUrl = `${parseUrl.protocol}//${parseUrl.host}`
+    const rawPath = url.replace(baseUrl, '')
+    let decodedPath = rawPath.split('/').map(decodeURIComponent).join('/');
+    let encodedPath = decodedPath.split('/').map(encodeURIComponent).join('/');
+    let fullUrl = `${baseUrl}${encodedPath}`;
+  return fullUrl
+}
+
+function handleArtcileData() {
+  const data = this.articleData
+  
+  this.articles = Object.keys(data).reduce((pre, cur) => {
+    const dataSet = data[cur].map(el => {
+
+    return {
+      ...el,
+      link: convertLinkURL(el.link),
+    }})
+    return ({ [cur]: dataSet, ...pre }) 
+  })
+  
+  if (data.hasOwnProperty("C#")) { 
+    data['CSharp'] = data['C#'] 
+    delete data['C#']
+  }
+
+  if (data.hasOwnProperty("Visual C++")) {
+    data['VCpp'] = data['Visual C++']
+    delete data['Visual C++']
+  }
 }
 
 export const useArticleStore = defineStore('articleStore', {
@@ -14,6 +48,7 @@ export const useArticleStore = defineStore('articleStore', {
     tags: null,
     articleCount: 0,
     currentPage: 1,
+    articles: null
   }),
   actions: {
     async fetchArticleData() {
@@ -21,6 +56,7 @@ export const useArticleStore = defineStore('articleStore', {
         const { data } = await useFetch('https://bingfenghung.github.io/DevArticles/articles.json')        
         this.articleData = data
         getArticleCount.call(this)
+        handleArtcileData.call(this)
       } catch (error) {
         console.log("Error fetch: ", error)
       }
